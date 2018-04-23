e1ef9ae1f2102f9605b758f322ddd7367b998948
gamgine <victor.fromentin95@ynov.com>
2018-04-23 16:08:52 +0200

    data serv req

---
+++ b2-aplication-mobile/b2-aplication-mobile/Services/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace b2_aplication_mobile.Services
+{
+    class DataService
+    {
+        public static async Task<dynamic> getDataFromService(string queryStyring)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryStyring);
+            if( response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                dynamic data = JsonConvert.DeserializeObject(json);
+                return data;
+            }
+            return null;
+        }
+    }
+}
